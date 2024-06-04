736109e61bca41c9500b632474cc07161dcf77cf
warlock <fecypher@gmail.com>
2024-06-04 17:19:06 +0100

    added niofew

--- hooks/useChatSupport.ts
+++ hooks/useChatSupport.ts
@@ -6,9 +6,10 @@ export const useChatSupport = () => {
   useEffect(() => {
     const script = document.createElement("script");
     script.async = true;
-    // script.src = "//code.tidio.co/km2upoiehybx9p9w5joga2e8lew0alxc.js";
+    script.src = "//code.jivosite.com/widget/TIBBEEKV0u";
 
     document.body.appendChild(script);
   }, []);
 };
+
 
